265b36e5102dd32beaaa0f3d7ea68ea9d5e03129
mtrPooh <it-exxpert@yandex.ru>
2024-11-28 14:05:10 +0500

    Added comments to code

--- conftest.py
+++ conftest.py
@@ -4,18 +4,28 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
+    """Опции командной строки.
+        В командную строку передается параметр вида '--language=ru'
+        """
     parser.addoption('--language', action='store', default=None,
                      help="Set language: ru, en, de etc.")
 
 @pytest.fixture(scope="function")
 def browser(request):
+    # В переменную language передается параметр из командной строки
     language = request.config.getoption("language")
+
     if language != None:
         print("\nstart chrome browser for test..")
+
+        # Инициализируются опции браузера
         options = Options()
+
+        # В опции вебдрайвера передаем параметр из командной строки
         options.add_experimental_option('prefs', {'intl.accept_languages': language})
         browser = webdriver.Chrome(options=options)
     else:
+        # Выводим ошибку при отсутствии параметра --language
         raise pytest.UsageError("--language should be site language: ru, en, de etc.")
     yield browser
     print("\nquit browser..")
--- test_items.py
+++ test_items.py
@@ -5,19 +5,27 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 class TestSiteLanguageInterface():
-
+    # Тест наличия кнопки добавления товара в корзину
     def test_add_to_cart_button_is_present(self, browser):
+        # Открываем страницу
         browser.get('https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
 
+        # Таймаут 30 сек для визуальной проверки соответствия заданного языка при запуске теста
+        # и языка страницы
         time.sleep(30)
 
         try:
+            # Ждем появления кнопки добавления товара в корзину 10 сек
             WebDriverWait(browser, 10).until(
                 EC.visibility_of_element_located((By.CSS_SELECTOR, "button.btn-add-to-basket"))
             )
+
+            # Флаг обнаружения кнопки добавления товара в корзину
             button_is_present = True
         except:
+            # Флаг отсутствия кнопки добавления товара в корзину
             button_is_present = False
 
+        # Проверяем наличие кнопки добавления товара в корзину
         assert button_is_present is True, 'Add to cart button not found!'
 
